a64b2a379efe466972702cb108ab61a31a06da26
Liam <liamcannotcode@gmail.com>
2025-06-18 19:43:33 +0200

    poison

--- l1/var/file_1.py
+++ l1/var/file_1.py
@@ -1,8 +1,20 @@
 health = 1000
-my_height = 100
-my_name = "jack"
-
-print(my_height)
-print(my_name)
 print(health)
 
+health = health - 100
+print(health)
+
+health = health - 100
+print(health)
+
+health = health - 100
+print(health)
+
+health = health - 100
+print(health)
+
+health = health - 100
+print(health)
+
+health = health - 100
+print(health)
